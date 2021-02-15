f2dfe4257b491e01d08e5a6c4191b0bf459204a3
SinBar <varka.sinitsckaya@yandex.ru>
2021-02-15 18:19:43 +0300

    Add files via upload

---
+++ OOP2LabaBankAccount/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP2LabaBankAccount
+{
+    enum AccountType {Checking, Deposit}
+    class BankAccount
+    {
+        static void Main(string[] args)
+        {
+            AccountType goldAccount = AccountType.Checking;
+            AccountType platinumAccount = AccountType.Deposit;
+            Console.WriteLine(" goldAccount = {0},", goldAccount);
+            Console.WriteLine(" platinumAccount = {0}", platinumAccount);
+        }
+    }
+}
---
+++ OOP2LabaStructType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP2LabaStructType
+{
+    enum AccountType { Checking, Deposit }
+    class BankAccountProgramm
+    {
+        public struct BankAccount
+        {
+            public long accNo;
+            public decimal accBal;
+            public AccountType accType;
+
+        }
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+            goldAccount.accNo = 809201865015910;
+            goldAccount.accBal = 10100100100100100100.35m;
+            goldAccount.accType = AccountType.Checking;
+            Console.WriteLine("Account number: {0}", goldAccount.accNo);
+            Console.WriteLine("Account type: {0}", goldAccount.accType);
+            Console.WriteLine("Account balance: {0:C2}", goldAccount.accBal);
+        }
+    }
+}
